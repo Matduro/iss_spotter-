82ba3f9925f5979d232dadcfad1bdc8cbfa50592
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-12 05:30:04 +0000

    Promises to the ISS files

--- index2.js
+++ index2.js
@@ -1,61 +1,14 @@
-// The code below is temporary and can be commented out.
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss2');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const printPassTimes = require('./index');
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
 
 
-/**
- * Input:
- *   Array of data objects defining the next fly-overs of the ISS.
- *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns:
- *   undefined
- * Sideffect:
- *   Console log messages to make that data more human readable.
- *   Example output:
- *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
- */
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
-
-
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned IP:' , ip);
-
-  fetchCoordsByIP(ip, (error, coordinates) => {
-    if (error) {
-      console.log("It didn't work!" , error);
-      return;
-    }
-  
-    console.log('It worked! Returned coordinates:' , coordinates);
-
-    fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
-      if (error) {
-        console.log("It didn't work!" , error);
-        return;
-      }
-    
-      console.log('It worked! Returned flyover times:' , passTimes);
-
-      nextISSTimesForMyLocation((error, passTimes) => {
-        if (error) {
-          return console.log("It didn't work!", error);
-        }
-        // success, print out the deets!
-        console.log(passTimes);
-      });
-    });
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
-});
-
-
